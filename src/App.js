105d3608dd08bce021116af1da30f2f0436bfe7b
neelendrarerddy9001 <neelendrareddy123@gmail.com>
2024-11-11 15:53:52 +0100

    styles added component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+// import { ToastContainer } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+
 function App() {
   return (
-    <div className="App">
-      <h1>React Firebase Deployment</h1>
-    </div>
+    <>
+      <div className="App">
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <h3>React Firebase Auth</h3>
+            <div className="login-app">
+              <div className="input-box">
+                <label htmlFor="name">Username</label>
+                <input type="text" placeholder="Enter your name" />
+              </div>
+              <div className="input-box">
+                <label htmlFor="name">Email</label>
+                <input type="email" placeholder="Enter your email" />
+              </div>
+              <button className="btn">Submit</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
